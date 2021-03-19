b30ed8fc7af8e44a714691a2984e606861975509
thesteau <25945183+thesteau@users.noreply.github.com>
2021-03-19 15:13:06 -0700

    Conversion of another new version of the program.

---
+++ Mail Program Files/_Version3 OOP.py
@@ -0,0 +1,12 @@
+## Version 3 Refactoring
+# By Steven Au
+# Purpose: For use in an office environment for basic email distribution based on data sheet values.
+
+# Import
+
+def main():
+    """ """
+    pass
+
+if __name__ == "__main__":
+    main()
