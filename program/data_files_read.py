13dced6f98dcf0068e35a5732e023d6f4050c68e
thesteau <25945183+thesteau@users.noreply.github.com>
2021-09-05 21:20:06 -0700

    Conversion of another new version of the program.

--- program/data_files_read.py
+++ program/data_files_read.py
@@ -54,35 +54,65 @@ class ReadMailing:
     # Set the dataframe values
     def process_data(self):
         """ Processes the data accordingly per the file extension.
-            Note: Excel files require the "tabs" to be specified in pandas.
+            Sets the self._pan_data to the loaded pandas data.
             Returns:
                 self._pan_data = pandas dataframes of the file data entered.
         """
-        if self._file_path is None:
+        if self._file_path is None:  # No file path is currently stored
             print('No file exists to process the data.')
             return
-        elif self._file_path.endswith('.xlsx'):
-            # Recall that excel has multiple tabs
-            pd_dat = self.excel_to_df()
-        elif self._file_path.endswith('.csv'):
-            pd_dat = self.csv_to_df()
-        # Further extension paths can be added here.
 
-        self._pan_data = pd_dat
+        # Header row will be skipped.
+        self._pan_data = self.data_to_df(self._file_path)
         return self._pan_data
 
-    # Excel
-    def excel_to_df(self):
+    # Data file to pandas dataframe
+    def data_to_df(self, file_path=None, skip_rows=0):
+        """ Read and load the data file into a pandas dataframe.
+            Note: this method was intended to be "flexible" to load any file into a pandas dataframe on demand.
+        """
+        if file_path is None:
+            print('No data file to process into a dataframe.....')
+            return
+        elif file_path.endswith(".xlsx"):
+            df = self.excel_processing(file_path, skip_rows)
+        elif file_path.endswith('.csv'):
+            df = self.csv_processing(file_path, skip_rows)
+        else:  # File is currently unsupported.
+            print('Please add the corresponding method for this file extension.')
 
-        # try except
-        return 1
+        df.fillna(self._the_word)
+        return df.values.tolist()
 
-    # CSV (Comma Separated Variable)
-    def csv_to_df(self):
-        return 1
+    def excel_processing(self, file_path, skip_rows):
+        """ Processes the excel file into a pandas dataframe.
+        """
+        excel_file = pd.ExcelFile(file_path)
 
-    # Further file types to dataframe methods can be added here.
+        try:
+            print('Note: The tab name is case sensitive, please ensure that it is correct.')
+            sheet_name = input('Enter the tab name to be read. > ')
+            the_dat = pd.read_excel(excel_file, sheet_name, skiprows=skip_rows)
+            print('Excel file read successfully!\n')
+            return the_dat
+        except:
+            print('The tab name is incorrect.')
+            print('Please check the file again for the specific tab name.')
+            print('Tip: Copy and paste the tab name.')
+            return self.excel_processing(file_path, skip_rows)
 
+    def csv_processing(self, file_path, skip_rows):
+        """ Processes a CSV file into a pandas dataframe.
+        """
+        the_dat = pd.read_csv(file_path, skiprows=skip_rows)
+        return the_dat
+
+    # Add additional filetype processing here.
+    def tsv_processing(self, file_path, skip_rows):
+        """ Processes a TSV file into a pandas dataframe.
+        """
+        the_dat = pd.read_csv(file_path, sep='\t', skiprows=skip_rows)
+        return the_dat
 
 
 test = ReadMailing()
